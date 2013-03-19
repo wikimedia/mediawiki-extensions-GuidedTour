1009a2fee752aba7db7f25f2e6c050a57df77007
Matthew Flaschen <mflaschen@wikimedia.org>
2013-03-18 22:05:10 -0400

    Copy jshint from core and update accordingly.
    * Only modification is to remove nomen due to guiders identifiers
    with a leading underscore
    
    Change-Id: I98822f67317bbb014925b7427db25539c40a6628

--- modules/externals/mediawiki.libs.guiders/mediawiki.libs.guiders.exposeGuiders.js
+++ modules/externals/mediawiki.libs.guiders/mediawiki.libs.guiders.exposeGuiders.js
@@ -1,8 +1,13 @@
+/*global guiders: false */
 ( function ( mw ) {
+
 	/* This is in the same module as the guiders code from the submodule.  Capture it
 	 * in mw.libs
 	 * (per https://www.mediawiki.org/wiki/Manual:Coding_conventions/JavaScript#Globals)
 	 * before we leave the module/anonymous function.
+	 *
+	 * It is not actually global in production mode; that just suppresses an
+	 * inapplicable jshint warning on this file.
 	 */
 	mw.libs.guiders = guiders;
 }( mediaWiki ) );
