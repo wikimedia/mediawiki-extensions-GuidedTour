e7e5cde0d55e0088094a91c09078ea5bf576244f
Sam Smith <git@samsmith.io>
2014-05-20 15:40:20 +0100

    Update firstedit
    Use the new GuidedTour API to define the firstedit tour.
    
    Change-Id: I47bc96a1f005f334146c74d7fef6bbd7b757912a

--- modules/tours/firstedit.js
+++ modules/tours/firstedit.js
@@ -2,7 +2,7 @@
 // Designed to work on any Wikipedia article, and can work for other sites with minor message changes.
 
 ( function ( window, document, $, mw, gt ) {
-	var hasEditSection;
+	var hasEditSection, tour;
 
 	function shouldShowForPage() {
 		// Excludes pages outside the main namespace and pages with editing restrictions
@@ -18,59 +18,81 @@
 
 	hasEditSection = $( '.mw-editsection' ).length > 0;
 
-	gt.defineTour( {
+	tour = new gt.TourBuilder( {
 		name: 'firstedit',
-		shouldLog: true,
-		steps: [ {
-			titlemsg: 'guidedtour-tour-firstedit-edit-page-title',
-			descriptionmsg: 'guidedtour-tour-firstedit-edit-page-description',
-			attachTo: '#ca-edit',
-			position: 'bottom',
-			// TODO (mattflaschen, 2013-09-03): After GuidedTour API enhancements, try to replace
-			// section-related shouldSkip and onclick code with proceedTo.
-			shouldSkip: gt.isEditing,
-			buttons: [ {
-				namemsg: hasEditSection ? 'guidedtour-next-button' : 'guidedtour-okay-button',
-				onclick: function () {
-					if ( hasEditSection ) {
-						mw.libs.guiders.next();
-					} else {
-						mw.libs.guiders.hideAll();
-					}
+		shouldLog: true
+	} );
+
+	tour.firstStep( {
+		name: 'intro',
+		titlemsg: 'guidedtour-tour-firstedit-edit-page-title',
+		descriptionmsg: 'guidedtour-tour-firstedit-edit-page-description',
+		attachTo: '#ca-edit',
+		position: 'bottom',
+		buttons: [ {
+			action: hasEditSection ? 'next' : 'okay',
+			onclick: function() {
+				if ( hasEditSection ) {
+					mw.libs.guiders.next();
+				} else {
+					mw.libs.guiders.hideAll();
 				}
-			} ],
-			allowAutomaticOkay: false
-		}, {
-			titlemsg: 'guidedtour-tour-firstedit-edit-section-title',
-			descriptionmsg: 'guidedtour-tour-firstedit-edit-section-description',
-			position: 'right',
-			attachTo: '.mw-editsection',
-			autoFocus: true,
-			width: 300,
-			shouldSkip: function () {
-				return gt.isEditing() || $( '.mw-editsection' ).length === 0;
 			}
-		}, {
-			titlemsg: 'guidedtour-tour-firstedit-preview-title',
-			descriptionmsg: 'guidedtour-tour-firstedit-preview-description',
-			attachTo: '#wpPreview',
-			autoFocus: true,
-			position: 'top',
-			closeOnClickOutside: false,
-			shouldSkip: function() {
-				return !gt.isEditing();
-			}
-		},  {
-			titlemsg: 'guidedtour-tour-firstedit-save-title',
-			descriptionmsg: 'guidedtour-tour-firstedit-save-description',
-			attachTo: '#wpSave',
-			autoFocus: true,
-			position: 'top',
-			closeOnClickOutside: false,
-			shouldSkip: function() {
-				return !gt.isReviewing();
-			}
-		} ]
+		} ],
+		allowAutomaticOkay: false
+	} )
+	.transition( function () {
+		if ( gt.isEditing() ) {
+			return 'preview';
+		}
+	} )
+	.next( 'editSection' );
+
+	tour.step( {
+		name: 'editSection',
+		titlemsg: 'guidedtour-tour-firstedit-edit-section-title',
+		descriptionmsg: 'guidedtour-tour-firstedit-edit-section-description',
+		position: 'right',
+		attachTo: '.mw-editsection',
+		autoFocus: true,
+		width: 300
+	} )
+	.transition( function () {
+		if ( gt.isEditing() ) {
+			return 'preview';
+		} else if ( !hasEditSection ) {
+			return gt.TransitionAction.HIDE;
+		}
+	} );
+
+	tour.step( {
+		name: 'preview',
+		titlemsg: 'guidedtour-tour-firstedit-preview-title',
+		descriptionmsg: 'guidedtour-tour-firstedit-preview-description',
+		attachTo: '#wpPreview',
+		autoFocus: true,
+		position: 'top',
+		closeOnClickOutside: false
+	} )
+	.transition( function () {
+		if ( gt.isReviewing() ) {
+			return 'save';
+		}
+	} );
+
+	tour.step( {
+		name: 'save',
+		titlemsg: 'guidedtour-tour-firstedit-save-title',
+		descriptionmsg: 'guidedtour-tour-firstedit-save-description',
+		attachTo: '#wpSave',
+		autoFocus: true,
+		position: 'top',
+		closeOnClickOutside: false
+	} )
+	.transition( function () {
+		if ( !gt.isReviewing() ) {
+			return gt.TransitionAction.END;
+		}
 	} );
 
 } (window, document, jQuery, mediaWiki, mediaWiki.guidedTour ) );
