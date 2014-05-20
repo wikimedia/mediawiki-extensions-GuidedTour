6af73b7edf01409169f70fb0d3d06854a78e89ed
Rob Moen <rmoen@wikimedia.org>
2014-05-20 14:06:16 -0700

    Update test tour to use the new non-linear tour API
    Change-Id: I20ce0338ad693ff85933757424faaf92e7c34daf

--- modules/tours/test.js
+++ modules/tours/test.js
@@ -3,52 +3,55 @@
  */
 ( function ( window, document, $, mw, gt ) {
 
-// XXX (mattflaschen, 2012-01-02): See GuidedTourHooks.php
-var pageName = mw.config.get( 'wgGuidedTourHelpGuiderUrl' );
+	// XXX (mattflaschen, 2012-01-02): See GuidedTourHooks.php
+	var pageName = mw.config.get( 'wgGuidedTourHelpGuiderUrl' ),
+		tour;
 
-gt.defineTour( {
-	/*
-	 * This is the name of the tour.  It must be lowercase, without any hyphen (-) or
-	 * period (.) characters.
-	 *
-	 * If this is an on-wiki tour, it should match the MediaWiki page.  For instance,
-	 * if this were on-wiki, it would be MediaWiki:Guidedtour-tour-test.js
-	 *
-	 * The IDs below should use the same name in the middle (e.g. gt-test-2).
-	 */
-	name: 'test',
-
-	/*
-	 * This is a list of the tour steps, in order.
-	 */
-	steps: [ {
+	tour = new gt.TourBuilder( {
 		/*
-		 * Show overlay at start of tour
+		 * This is the name of the tour.  It must be lowercase, without any hyphen (-) or
+		 * period (.) characters.
+		 *
+		 * If this is an on-wiki tour, it should match the MediaWiki page.  For instance,
+		 * if this were on-wiki, it would be MediaWiki:Guidedtour-tour-test.js
+		 *
+		 * The IDs below should use the same name in the middle (e.g. gt-test-2).
 		 */
+		name: 'test'
+	} );
+
+	tour.firstStep( {
+		name: 'overlay',
 		titlemsg: 'guidedtour-tour-test-testing',
 		descriptionmsg: 'guidedtour-tour-test-test-description',
 		overlay: true,
 		buttons: [ {
 			action: 'next'
 		} ]
-	}, {
+	} )
+	.next( 'callout' );
+
+	tour.step( {
 		/*
 		 * Callout of left menu
 		 */
+		name: 'callout',
 		titlemsg: 'guidedtour-tour-test-callouts',
 		descriptionmsg: 'guidedtour-tour-test-portal-description',
-
 		// attachment
 		attachTo: '#n-portal a',
 		position: '3',
-
 		buttons: [ {
 			action: 'next'
 		} ]
-	}, {
+	} )
+	.next( 'description' );
+
+	tour.step( {
 		/*
 		 * Test out mediawiki description pages
 		 */
+		name: 'description',
 		titlemsg: 'guidedtour-tour-test-description-page',
 		description: pageName,
 
@@ -62,7 +65,6 @@ gt.defineTour( {
 		}, {
 			action: 'end'
 		} ]
-	} ]
-} );
+	} );
 
-} (window, document, jQuery, mediaWiki, mediaWiki.guidedTour ) );
+} ( window, document, jQuery, mediaWiki, mediaWiki.guidedTour ) );
