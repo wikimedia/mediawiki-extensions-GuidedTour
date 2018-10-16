29fe3da223d6ca132893ee77efe7de1be9e5b1fa
Kunal Mehta <legoktm@member.fsf.org>
2018-10-16 11:29:00 -0700

    Warn if legacy PHP entry point is used
    Bug: T140851
    Change-Id: I2831803a7990a112793ff1db7dcdf811ed63f365

--- GuidedTour.php
+++ GuidedTour.php
@@ -16,10 +16,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'GuidedTour' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['GuidedTour'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 	'Deprecated PHP entry point used for Renameuser extension. Please use wfLoadExtension instead, ' .
 	'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return true;
 } else {
 	die( 'This version of the GuidedTour extension requires MediaWiki 1.25+' );
