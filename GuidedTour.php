a63066b35caac59ac360af720faa99b3b558d6f7
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 20:46:01 +0200

    Fix extension name in deprecation note in the php entry point
    Change-Id: I38b432cffd58117f560a6dfad9a50e6dd7541d86

--- GuidedTour.php
+++ GuidedTour.php
@@ -17,7 +17,7 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['GuidedTour'] = __DIR__ . '/i18n';
 	wfWarn(
-	'Deprecated PHP entry point used for Renameuser extension. Please use wfLoadExtension instead, ' .
+	'Deprecated PHP entry point used for GuidedTour extension. Please use wfLoadExtension instead, ' .
 	'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	);
 	return true;
