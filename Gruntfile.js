1d13bfe7d7e6109c01ed10e6012253dec305b174
Michael Große <mgrosse@wikimedia.org>
2025-06-20 18:37:11 +0200

    chore: factor eslint and stylelint out of Grunt
    This gets us closer to a conventional modern setup of our frontend
    linting and a step closer to ditching Grunt altogether. Now we depend a
    bit less on poorly maintained Grunt plugins. We continue to use Grunt
    for Wikimedia's homegrown banana-checker, because it seems to basically
    exist only as Grunt plugin.
    
    This also introduces npm-run-all to keep our "test" script in the
    package.json short.
    
    See If5da4a55f81683294b863dabf46aa46af1db1fca for a similar change
    
    Change-Id: I6e3e8254f76ea68db4879ecb9774a1e880e562ec

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,31 +3,12 @@
 module.exports = function ( grunt ) {
 	const conf = grunt.file.readJSON( 'extension.json' );
 
-	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
-	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
-		eslint: {
-			options: {
-				cache: true,
-				fix: grunt.option( 'fix' )
-			},
-			all: [ '.' ]
-		},
-		banana: conf.MessagesDirs,
-		stylelint: {
-			options: {
-				cache: true
-			},
-			all: [
-				'**/*.less',
-				'!node_modules/**',
-				'!vendor/**'
-			]
-		}
+		banana: conf.MessagesDirs
 	} );
 
-	grunt.registerTask( 'test', [ 'eslint', 'banana', 'stylelint' ] );
-	grunt.registerTask( 'default', [ 'test' ] );
+	grunt.registerTask( 'test', [ 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
 };
