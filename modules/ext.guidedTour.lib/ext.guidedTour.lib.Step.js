8b73fca0f0f8095f843d9fd31016abe1fe921f6d
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2026-09-03 12:07:11 +0000

    build: Updating npm dependencies
    * eslint-config-wikimedia: ^0.31.0 → 0.32.4
    * grunt: 1.6.2 → 1.6.3
    * svgo: 3.3.3 → 3.3.5
    * brace-expansion: 1.1.13, 2.1.4 → 1.1.18, 2.1.4
      * https://github.com/advisories/GHSA-3jxr-9vmj-r5cp
      * https://github.com/advisories/GHSA-mh99-v99m-4gvg
      * https://github.com/advisories/GHSA-rgw5-rvv9-x895
    * fast-uri: 3.1.2 → 3.1.7
      * https://github.com/advisories/GHSA-4c8g-83qw-93j6
      * https://github.com/advisories/GHSA-7p8r-x3mc-p8w7
      * https://github.com/advisories/GHSA-f65p-4m7j-42xc
      * https://github.com/advisories/GHSA-fph4-wmhf-6fwf
      * https://github.com/advisories/GHSA-jqff-g426-hqxp
      * https://github.com/advisories/GHSA-v2hh-gcrm-f6hx
    * linkify-it: 5.0.1 → 5.0.2
      * https://github.com/advisories/GHSA-v245-v573-v5vm
    * nanoid: 3.3.11 → 3.3.18
      * https://github.com/advisories/GHSA-28wg-ghj8-5hjv
      * https://github.com/advisories/GHSA-2v37-7h3g-55p8
      * https://github.com/advisories/GHSA-xwg4-73v4-xw9w
    * postcss: 8.5.12 → 8.5.27
      * https://github.com/advisories/GHSA-fxqj-rqcc-2cmp
      * https://github.com/advisories/GHSA-r28c-9q8g-f849
    * postcss-selector-parser: 6.1.2, 7.1.0 → 6.1.4, 7.1.6
      * https://github.com/advisories/GHSA-w9m9-85wc-3x92
    * shell-quote: 1.8.4 → 1.10.0
      * https://github.com/advisories/GHSA-395f-4hp3-45gv
    
    Additional changes:
    * Disabled Composer audit.block-insecure option (T416518)
    
    Change-Id: If9aeed04f7f6ee1963a603fc6dba156f2a08f56d

--- modules/ext.guidedTour.lib/ext.guidedTour.lib.Step.js
+++ modules/ext.guidedTour.lib/ext.guidedTour.lib.Step.js
@@ -202,7 +202,6 @@
 		// TODO: Refactor how namemsg is handled, for code reuse.
 		if ( button.namemsg ) {
 			messageKey = button.namemsg;
-			// eslint-disable-next-line mediawiki/msg-doc
 			button.name = mw.message( button.namemsg ).parse();
 			delete button.namemsg;
 		}
@@ -212,7 +211,6 @@
 			hasIcon = false;
 		} else {
 			messageKey = messageKeyMapping[ button.action ];
-			// eslint-disable-next-line mediawiki/msg-doc
 			button.name = mw.message( messageKey ).parse();
 			hasIcon = true;
 		}
@@ -319,7 +317,6 @@
 		};
 
 		if ( button.namemsg ) {
-			// eslint-disable-next-line mediawiki/msg-doc
 			button.name = mw.message( button.namemsg ).parse();
 			delete button.namemsg;
 		}
@@ -341,7 +338,6 @@
 	 */
 	Step.prototype.modifyCustomButton = function ( button ) {
 		if ( button.namemsg ) {
-			// eslint-disable-next-line mediawiki/msg-doc
 			button.name = mw.message( button.namemsg ).parse();
 			delete button.namemsg;
 		}
@@ -734,13 +730,11 @@
 		};
 
 		if ( options.titlemsg ) {
-			// eslint-disable-next-line mediawiki/msg-doc
 			options.title = mw.message( options.titlemsg ).parse();
 		}
 		delete options.titlemsg;
 
 		if ( options.descriptionmsg ) {
-			// eslint-disable-next-line mediawiki/msg-doc
 			options.description = mw.message( options.descriptionmsg ).parse();
 		}
 		delete options.descriptionmsg;
--- modules/mediawiki.libs.guiders/mediawiki.libs.guiders.js
+++ modules/mediawiki.libs.guiders/mediawiki.libs.guiders.js
@@ -174,7 +174,6 @@ mw.libs.guiders = ( function () {
 			);
 
 			if ( typeof thisButton.classString !== 'undefined' && thisButton.classString !== null ) {
-				// eslint-disable-next-line mediawiki/class-doc
 				$thisButtonElem.addClass( thisButton.classString );
 			}
 
@@ -477,7 +476,6 @@ mw.libs.guiders = ( function () {
 		// eslint-disable-next-line no-jquery/no-fade
 		$( '#guider_overlay' ).fadeIn( 'fast' ).each( function () {
 			if ( overlayClass ) {
-				// eslint-disable-next-line mediawiki/class-doc
 				$( this ).addClass( overlayClass );
 			}
 		} );
@@ -529,7 +527,6 @@ mw.libs.guiders = ( function () {
 		};
 
 		// Classes documented above
-		// eslint-disable-next-line mediawiki/class-doc
 		$myGuiderArrow.addClass( newClass[ position ] );
 
 		const myHeight = myGuider.elem.innerHeight();
@@ -591,7 +588,6 @@ mw.libs.guiders = ( function () {
 		// Assign animation class for myGuider
 		if ( position !== 0 ) {
 			// Classes documented above
-			// eslint-disable-next-line mediawiki/class-doc
 			myGuider.elem.addClass( classMap[ position ] );
 		}
 	};
@@ -693,7 +689,6 @@ mw.libs.guiders = ( function () {
 		// eslint-disable-next-line no-jquery/variable-pattern
 		myGuider.elem = $guiderElement;
 		if ( typeof myGuider.classString !== 'undefined' && myGuider.classString !== null ) {
-			// eslint-disable-next-line mediawiki/class-doc
 			myGuider.elem.addClass( myGuider.classString );
 		}
 		myGuider.elem.css( 'width', myGuider.width + 'px' );
